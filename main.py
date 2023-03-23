d5963dc4108ba0aa30e4e748d9b29a9d8b219da4
Will Shiflett <wshiflett@baosystems.com>
2023-03-22 22:52:22 -0400

    Adding "AI" and different ship renderings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 class GameBoard(object):
 
@@ -23,8 +24,6 @@ class GameBoard(object):
         return all([b.is_destroyed() for b in self.battleships])
             
         #for each battleship, is it destroyed?
-
-
 
 class Shot(object):
 
@@ -60,10 +59,11 @@ class Battleship(object):
                 el = (head[0] - i, head[1])
             body.append(el)
 
-        return Battleship(body)
+        return Battleship(body, direction)
 
-    def __init__(self, body):
+    def __init__(self, body, direction):
         self.body = body
+        self.direction = direction
         self.hits = [False] * len(body)
 
     def body_index(self, location):
@@ -74,6 +74,11 @@ class Battleship(object):
         
     def is_destroyed(self):
         return all(self.hits)
+
+class Player(object):
+    def __init__(self, name, shot_f):
+        self.name = name
+        self.shot_f = shot_f
 
 def render(game_board, show_battleships=False):
     header = "+" + "-" * game_board.width + "+"
@@ -89,8 +94,25 @@ def render(game_board, show_battleships=False):
     #replace the value "None" with "O" wherever there are battleships
     if show_battleships:
         for b in game_board.battleships:
-            for x,y in b.body:
-                board[x][y] = "O"
+            for i, (x,y) in enumerate(b.body):
+                if b.direction == 'N':
+                    chs = ('v','|','^')
+                elif b.direction == 'S':
+                    chs = ('^','|','v')
+                elif b.direction == 'W':
+                    chs = ('>','=','<')
+                elif b.direction == 'E':
+                    chs = ('<','=','>')
+                else:
+                    raise 'Unknown direction'
+
+                if i == 0:
+                    ch = chs[0]
+                elif i == len(b.body) - 1:
+                    ch = chs[2]
+                else:
+                    ch = chs[1]
+                board[x][y] = ch
     
     #rplace the value "None" or "O" with "X" whenever there is a shot
     for sh in game_board.shots:
@@ -111,12 +133,30 @@ def render(game_board, show_battleships=False):
 
     print(header)
 
+def get_random_ai_shot(game_board):
+    # random.randint(a, b)
+    x = random.randint(0, game_board.width - 1)
+    y = random.randint(0, game_board.height - 1)
+    return (x, y)
+
+#Todo, make actual clever AI
+def get_clever_ai(game_board):
+    return(0,0)
+
+def get_human_shot(game_board):
+    inp = input("Where do you want to shoot?\n")
+    xstr, ystr = inp.split(",")
+    x = int(xstr)
+    y = int(ystr)
+
+    return(x, y)
+
 if __name__ == "__main__":
 
     battleships = [
         Battleship.build((1,1), 2, "N"),
-        # Battleship.build((5,8), 5, "N"),
-        # Battleship.build((2,3), 4, "E")
+        Battleship.build((5,8), 5, "N"),
+        Battleship.build((2,3), 4, "E")
     ]
 
     game_boards = [
@@ -124,9 +164,10 @@ if __name__ == "__main__":
         GameBoard(copy.deepcopy(battleships), 10, 10)
     ]
 
-    player_names = [
-        "Frank",
-        "Alice"
+    # Pass in the name of the player and the shot getting function itself
+    players = [
+        Player("Rob", get_human_shot),
+        Player("Alice", get_random_ai_shot)
     ]
 
     offensive_idx = 0
@@ -136,17 +177,18 @@ if __name__ == "__main__":
 
         defensive_board = game_boards[defensive_idx] 
 
-        print('%s YOUR TURN!' % player_names[offensive_idx])
-        inp = input("Where do you want to shoot?\n")
-        xstr, ystr = inp.split(",")
-        x = int(xstr)
-        y = int(ystr)
+        offensive_player = players[offensive_idx]
 
-        defensive_board.take_shot((x,y))
-        render(defensive_board)
+        print('%s YOUR TURN!' % offensive_player.name)
+
+        shot_location = offensive_player.shot_f(defensive_board)
+
+        defensive_board.take_shot(shot_location)
+
+        render(defensive_board, True)
 
         if defensive_board.is_game_over():
-            print('%s WINS!' % player_names[offensive_idx])
+            print('%s WINS!' % offensive_player.name)
             break
 
         offensive_idx = defensive_idx
